e2d9f19f76a82ca664b4d5eef126164de591b135
ivopeu <ivopedro.f@gmail.com>
2019-04-10 01:12:05 -0300

    Adciona variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -34,4 +34,4 @@ app.use(express.urlencoded({
 app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(require('./routes.js'));
 
-server.listen(8888);
+server.listen(process.env.PORT || 8888);
